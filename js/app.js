9f9ee226dd337c164351518d527b0a5ad98afc1b
VitaliyNosov <webdeveloperstudio2017@gmail.com>
2020-11-22 17:20:00 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // fotorama customization
+
+    $('.fotorama').fotorama({
+        width: '700',
+        hash: true,
+        maxheight: '100%',
+        nav: 'thumbs',
+        allowfullscreen: 'native',
+        keyboard: {space: true},
+        shadows: false,
+        fit: 'cover'
+    });
+
+    // fotorama customization
+
+});
+
+
+
